8dd7f7604e136c1790d5790ae38c6786673d4051
S. T. Welikanna <it22196910@my.sliit.lk>
2026-01-03 15:15:38 +0530

    Update MongoDB document serialization logic

--- backend/learn-service/app/models/common.py
+++ backend/learn-service/app/models/common.py
@@ -1,3 +1,3 @@
-def strip_mongo_id(doc: dict) -> dict:
-    """Return a copy of a MongoDB document without the _id field."""
-    return {k: v for k, v in doc.items() if k != "_id"}
+def serialize_mongo_doc(doc: dict) -> dict:
+    doc["_id"] = str(doc["_id"])
+    return doc
